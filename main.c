ca5fa813f89238622ade1cd9fc464380045535f5
Yoel <yoelridgway@gmail.com>
2022-08-23 18:45:04 +0200

    refactored the code a bunch but still have a small problem with the processing of numbers in char **av to the stack array

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: myaccount <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 15:02:44 by myaccount         #+#    #+#             */
-/*   Updated: 2022/08/22 21:14:52 by myaccount        ###   ########.fr       */
+/*   Updated: 2022/08/23 18:43:33 by myaccount        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,52 @@ void	perform_operations(char *op, t_stack *stack_a, t_stack *stack_b)
 	write(1, "\n", 1);
 }
 
+t_stack	*initialize_stack(char **av)
+{
+	int	i;
+	t_stack	*stack;
+
+	i = 0;
+	stack = malloc(sizeof (t_stack));
+	stack->max = atoi(av[i]);
+	stack->array = (int *)malloc(stack->max * sizeof (int));
+	while (av[++i] && av[i][0] >= '0' && av[i][0] <= '9')
+	{
+		stack->array[i - 2] = atoi(av[i]);
+		stack->size += 1;
+	}
+	return (stack);
+}
+
+t_stack	*initialize_empty_stack(int n)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof (t_stack));
+	stack->max = n;
+	stack->array = (int *)malloc(stack->max * sizeof (int));
+	return (stack);
+}
+
+void	execute_operations(char **av, t_stack *stack_a, t_stack *stack_b)
+{
+	int	i;
+
+	i = 0;
+	write(1, "before:\n", 8);
+	display_stacks(stack_a, stack_b);
+	write(1, "\n", 1);
+	while (av[i][0] >= '0' && av[i][0] <= '9')
+		i++;
+	while (av[i] && av[i][0] >= 'p' && av[i][0] <= 's')
+	{
+		perform_operations(av[i], stack_a, stack_b);
+		i++;
+	}
+	write(1, "\nafter:\n", 8);
+	display_stacks(stack_a, stack_b);
+}
+
 int	main(int ac, char **av)
 {
 	int	i;
@@ -93,24 +139,11 @@ int	main(int ac, char **av)
 		printf("not enough arguments!\n");
 		return (0);
 	}
-	stack_a = malloc(sizeof (t_stack));
-	stack_b = malloc(sizeof (t_stack));
-	stack_a->max = atoi(av[i]);
-	stack_a->array = (int *)malloc(stack_a->max * sizeof (int));
-	while (av[++i][0] >= '0' && av[i][0] <= '9')
-	{
-		stack_a->array[i - 2] = atoi(av[i]);
-		stack_a->size += 1;
-	}
-	write(1, "before:\n", 8);
-	display_stacks(stack_a, stack_b);
-	write(1, "\n", 1);
-	while (av[i])
-	{
-		perform_operations(av[i], stack_a, stack_b);
-		i++;
-	}
-	write(1, "\nafter:\n", 8);
-	display_stacks(stack_a, stack_b);
+	stack_a = initialize_stack(av + 2);
+	stack_b = initialize_empty_stack(100);
+	if (av[i][0] == 'e')
+		execute_operations(av + 2, stack_a, stack_b);
+//	else if (av[i][0] == 'r')
+//		three_number_algo(av + 2, stack_a, stack_b);
 	return (0);
 }
