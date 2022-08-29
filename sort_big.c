efc87ada122852207d8827d2ba96cf4a508084ec
Yoel <yoelridgway@gmail.com>
2022-08-30 00:15:48 +0200

    mapped out the main logic of my big sort algo

--- sort_big.c
+++ sort_big.c
@@ -6,13 +6,13 @@
 /*   By: myaccount <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 12:20:34 by myaccount         #+#    #+#             */
-/*   Updated: 2022/08/29 20:38:57 by myaccount        ###   ########.fr       */
+/*   Updated: 2022/08/30 00:15:03 by myaccount        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	split_median(t_stack *stack_a, t_stack *stack_b)
+void	split_median_lower(t_stack *stack_a, t_stack *stack_b)
 {
 	size_t	i;
 	int		median;
@@ -32,8 +32,40 @@ void	split_median(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
-void	sort_big(t_stack *stack_a, t_stack *stack_b)
+void	split_median_upper(t_stack *stack_a, t_stack *stack_b)
 {
-	split_median(stack_a, stack_b);
 }
 
+void	rot_to_max(t_stack *stack)
+{
+}
+
+int		moves_to(t_stack *stack, int value)
+{
+}
+
+void	sort_half(t_stack *stack_a, t_stack *stack_b)
+{
+	while (stack_b->size)
+	{
+		if (moves_to(stack_b, max) < moves_to(stack_b, min))
+		{
+			rot_to_max(stack_b);
+			ft_push(stack_b, stack_a);
+		}
+		else
+		{
+			rot_to_min(stack_b);
+			ft_push(stack_b, stack_a);
+			ft_ra(stack_a);
+		}
+	}
+}
+
+void	sort_big(t_stack *stack_a, t_stack *stack_b)
+{
+	split_median_lower(stack_a, stack_b);
+	sort_half(stack_a, stack_b);
+	split_median_upper(stack_a, stack_b);
+	sort_half(stack_a, stack_b);
+}
